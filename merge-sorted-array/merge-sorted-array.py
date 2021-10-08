68bf5de7f8e85493cdb68c63fc782cf2a680ba77
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-10-08 16:40:09 +0800

    Time: 36 ms (77.54%), Space: 14.3 MB (29.83%) - LeetHub

--- merge-sorted-array/merge-sorted-array.py
+++ merge-sorted-array/merge-sorted-array.py
@@ -1,9 +1,7 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i = 0
-        while(i < n):
+        for i in range(0, n):
             nums1[i + m] = nums2[i]
-            i += 1
         nums1.sort()
         """
         Do not return anything, modify nums1 in-place instead.
