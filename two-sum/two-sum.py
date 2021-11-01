af44a7151f98f5e02fb37157660bc659c3b2e1b5
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-01 17:26:07 +0800

    Time: 60 ms (82.79%), Space: 15.4 MB (19.97%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -9,3 +9,21 @@ class Solution:
                 return [i, seen[remaining]]
             else:
                 seen[value] = i
+                
+"""
+ range(len(list)) can be replaced by using "enumerate()" as seen below 
+"""               
+                
+                
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         seen = {}
+        
+#         for i in range(len(nums)):
+#             print(i, nums[i])
+#             remaining = target - nums[i]
+            
+#             if remaining in seen:
+#                 return [i, seen[remaining]]
+#             else:
+#                 seen[nums[i]] = i
