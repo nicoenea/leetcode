fb905e2ad5b273a6094e053d3be3efbf085b5f0a
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-04 00:08:00 +0800

    Update reshape-the-matrix.py

--- reshape-the-matrix/reshape-the-matrix.py
+++ reshape-the-matrix/reshape-the-matrix.py
@@ -1,7 +1,14 @@
 import numpy
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        """
+            Check if the size of the matrix  == size of the matrix to be returned
+            
+            len(mat) == number of rows (r)
+            len(mat[0]) == number of columns (c)
+        """
         if r*c == len(mat) * len(mat[0]):
+            print(r*c, len(mat)*len(mat[0]))
             return numpy.reshape(mat,(r,c))
         else:
             return mat
