1f5d4b6528a67f4eef2551c612e67925990f75ca
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-04 23:44:14 +0800

    Update binary-search.py

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -1,9 +1,16 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        matchCount = 0
-        for i, _ in enumerate(nums):
-            if nums[i] == target:
+        """
+        Approach #1:
+        1. define 'matchCount' as a flag to tell if there was a match to 'target' in our list.
+        2. Iterate through list index by using enumerate() to get index
+        3. if nums[i] is equal to target, we increase matchCount to indicate that we have found a match
+        4. Once for loop is finished, check if there was a match, or else return -1
+        """
+        matchCount = 0 # 1
+        for i, _ in enumerate(nums): # 2
+            if nums[i] == target: # 3
                 matchCount += 1
                 return i
-        if matchCount == 0:
+        if matchCount == 0: #4
             return -1
