2bdcd262116caf7b6db327accd667cc0cd87b7c9
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-07 23:39:13 +0800

    Time: 32 ms (89.57%), Space: 14.3 MB (29.49%) - LeetHub

--- multiply-strings/multiply-strings.py
+++ multiply-strings/multiply-strings.py
@@ -1,7 +1,14 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        # Put all possible integers in a dictionary
-        intDict = {
+        """
+            1. Put all possible integers in a dictionary.
+            2. init integer variables to store our converted strings
+            3. iterate through the strings, and put all the digits back together as int 
+                3a. Multiply previous iteration by 10 to make space for the next digit
+            4. Return product of both ints, converted back to string
+        """
+        
+        intDict = { # 1
             '0' : 0,
             '1' : 1,
             '2' : 2,
@@ -13,15 +20,22 @@ class Solution:
             '8' : 8, 
             '9' : 9,
         }
-        #init integer variables to store our converted strings
-        int1 = 0
+        int1 = 0 #2
         int2 = 0
         
-        # iterate through string, and put all digits back together as int 
-        for char in num1:
-            int1 = 10 * int1 + intDict[char]
+        for char in num1: #3
+            print("before: ", int1)
+            int1 = intDict[char] + (10 * int1)
+            print("after: ", int1)
         for char in num2:
             int2 = 10 * int2 + intDict[char]
+        print(int1, int2)
+        return str(int1 * int2) #4
         
-        # return product of both ints, converted to string
-        return str(int1 * int2)
+        
+        """
+        Easy one-line Solution: 
+        (goes against the problem description but still accepted by judge)
+        """
+        #return str(int(num1) * int(num2))
+        
