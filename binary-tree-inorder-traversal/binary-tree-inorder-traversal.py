6309647e7465f35e743ca021154884f21e8b0422
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-09 23:37:48 +0800

    Time: 24 ms (96.83%), Space: 14.4 MB (13.93%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,16 +6,47 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        # Linear Time
+        
+        """
+        Iterative Solution:
+        
+            Since we are using a stack, we need to write it the other way around:
+                    instead of Left -> Val -> Right
+                    we write: Right -> Val -> Left
+            
+            Using isinstance() method to judge the type of the value 
+                that just popped from the stack.
+        """
+        stack = [root]
         output = []
-        
-        def helper(node):
-            if not node:
-                return
-            
-            helper(node.left)
-            output.append(node.val)
-            helper(node.right)
-            
-        
-        helper(root)
+        while stack:
+            tempNode= stack.pop()
+            if tempNode:
+                if isinstance(tempNode, TreeNode):
+                    
+                    stack.append(tempNode.right)
+                    stack.append(tempNode.val)
+                    stack.append(tempNode.left)
+                else:
+                    output.append(tempNode)
         return output
+        
+        
+        """
+        Recursive Solution:
+        """
+#         output = []
+        
+#         def helper(node):
+#             if not node:
+#                 return
+            
+#             helper(node.left)
+#             output.append(node.val)
+#             helper(node.right)
+            
+        
+#         helper(root)
+#         return output
