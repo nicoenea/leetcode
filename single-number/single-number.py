475c97bff42b896262d42705079787b70a56c4ae
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-15 00:26:38 +0800

    Time: 128ms (85.21%), Space: 16.8 MB (19.15%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,5 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        """
+        Beeg Brain Approach
+        """
+        return 2 * sum(set(nums)) - sum(nums)
+        """
+        First Approach:
+        
+        very ineffective, not linear time
+        """
         for num in nums:
             if nums.count(num) == 1:
                 return num
