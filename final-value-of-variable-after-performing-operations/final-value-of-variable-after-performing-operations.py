8201af78cf2e19b6528586eedee53287688a76d7
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-15 00:13:47 +0800

    Time: 28 ms (86.84%), Space: 14 MB (91.43%)

--- final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -1,29 +1,43 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
+        """
+        Better thought approach:
+            
+            iterate through list and check if '+' in operation, and add/subtract accordingly
+            Time: 28 ms (86.84%), Space: 14 MB (91.43%) 
+        """
+        var = 0
+        for op in operations:
+            if '+' in op:
+                var += 1
+            else: 
+                var -= 1
+        return var
         
         
         """
         Rookie approach:
         
-            Use dictionary and iterate array to decide which operation to apply to var
+            Use dictionary and iterate array to decide which operation to apply to var  
+            Time: 56 ms (44.33%), Space: 14.5 MB (17.05%) - LeetHub
         """
-        opDict = {
-            0 : '++X',
-            1 : 'X++',
-            2 : '--X',
-            3 : 'X--'
-        }
-        var = 0
-        for i in operations:
-            print(i)
-            if i == opDict[0]:
-                var += 1
-            elif i == opDict[1]:
-                var += 1
-            elif i == opDict[2]:
-                var -= 1
-            elif i == opDict[3]:
-                var -= 1
+#         opDict = {
+#             0 : '++X',
+#             1 : 'X++',
+#             2 : '--X',
+#             3 : 'X--'
+#         }
+#         var = 0
+#         for i in operations:
+#             print(i)
+#             if i == opDict[0]:
+#                 var += 1
+#             elif i == opDict[1]:
+#                 var += 1
+#             elif i == opDict[2]:
+#                 var -= 1
+#             elif i == opDict[3]:
+#                 var -= 1
                 
-        return var
+#         return var
                 
