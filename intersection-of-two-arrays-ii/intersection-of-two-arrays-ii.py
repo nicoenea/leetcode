512ad2e71f1def3e34d8cba224a871fa2ec5a2dc
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-17 00:43:14 +0800

    Time: 44 ms (89.15%), Space: 14.1 MB (97.80%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,5 +1,29 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        cnt1, cnt2 = Counter(nums1), Counter(nums2)
-               
-        return sum([[k] * min(n, cnt2[k]) for k, n in cnt1.items() if k in cnt2], [])
+
+        ## 2 Pointer Approach
+        """
+        Time Complexity:  O(NlogN)
+	    Space Complexity: O(1) 
+        """
+		
+        indx1 = 0
+        indx2 = 0
+        intersections = []
+        # Sort lists before putting through while loop to do 2 pointer approach
+        nums1.sort() # sort() uses Timsort Algorithm 
+        nums2.sort()
+        
+        while indx1 < len(nums1) and indx2 < len(nums2):
+            
+                
+            if nums1[indx1] < nums2[indx2]:
+                indx1 += 1
+                continue # add continue to make it check again after index+=1
+            if nums2[indx2] < nums1[indx1]:
+                indx2 += 1
+                continue
+            intersections.append(nums1[indx1])
+            indx1 += 1
+            indx2 += 1
+        return intersections
