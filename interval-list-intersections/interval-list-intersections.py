34a55515125316374c5924ef3d2b656c643b7eda
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-24 11:10:13 +0800

    Time: 148 ms (71.99%), Space: 15.1 MB (39.75%)

--- interval-list-intersections/interval-list-intersections.py
+++ interval-list-intersections/interval-list-intersections.py
@@ -1,31 +1,23 @@
 class Solution:
     
-    def findInterval(val1, val2):
-        pointer = 0
-        if val1[1] > val2[1]: 
-            pointer = 2
-        else: pointer = 1
-            
-        if val1[0] > val2[1] or val2[0] > val1[1]: # if the given closed intervals don't match, there is no interval
-            return [], pointer # returning empty 
-        
-        start = max(val1[0], val2[0]) #find start and end of interval
-        end  = min(val1[1], val2[1])
-        return [start, end], pointer
-    
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         ans = []
-        pt1 = pt2 = 0
+        ptr1, ptr2 = 0, 0
         
-        while pt1 < len(firstList) and pt2 < len(secondList): # iterate through both lists
+        while ptr1 < len(firstList) and ptr2 < len(secondList): # iterate through both lists
             
-            interval, pointer = Solution.findInterval(firstList[pt1], secondList[pt2]) # use function to find interval
+            start = max(firstList[ptr1][0], secondList[ptr2][0])
+            end = min(firstList[ptr1][1], secondList[ptr2][1])
             
-            if interval:  # append intervals to ans
-                ans.append(interval)
-            if pointer == 1: # increase pointer
-                pt1 += 1
-            else:
-                pt2 += 1
+            if start <= end: # if there is an interval, append it to list
+                ans.append([start,end])
+            if firstList[ptr1][1] < secondList[ptr2][1]: # move pointers accordingly
+                ptr1 += 1
+            else: 
+                ptr2 += 1
             
-        return ans
+        return ans 
+    
+    
+    # O(N+M) Time 
+    # O(1) Space
