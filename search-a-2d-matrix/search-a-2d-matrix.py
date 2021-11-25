4241be5b8ae7b3422ebb55238126dbc4c554ccf9
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-11-25 17:08:48 +0800

    Time: 51 ms (26.08%), Space: 14.7 MB (65.76%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.py
+++ search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,6 +1,6 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for row in matrix:
-            if target in row:
+        for row in matrix: # iterate through rows of matrix
+            if target in row: # if target in row, return true
                 return True
-        return False
+        return False # if finished iterating, it means we don't have target in matrix, so return False
