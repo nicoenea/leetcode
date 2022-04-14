76648ca61fc2c23bf91ea3f7bc193b0c8a5a4f6f
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2022-04-14 14:09:10 +0800

    Time: 80 ms (85.64%), Space: 16.4 MB (74.05%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        current_node = root
+        while current_node != None:
+            if current_node.val == val:
+                return current_node
+            elif current_node.val < val:
+                current_node = current_node.right
+            else:
+                current_node = current_node.left
+        return None
